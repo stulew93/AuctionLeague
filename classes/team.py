62e15c1e0013eea36d3d6f73723d6b3b0fd62f01
Stuart Lewis <stuart.lewis3110@gmail.com>
2022-06-04 23:12:47 +0100

    Add class description.

--- classes/team.py
+++ classes/team.py
@@ -1,6 +1,6 @@
 class Team:
     '''
-    The Team class is a shell for the Squad - set up liek this as in future I hope to add configurable elements
+    The Team class is a shell for the Squad - set up like this as in future I hope to add configurable elements
     (such as managers). The Team object will manage the budget for the team, including keeping track of the remaining
     budget, and whether the team is complete.
     '''
