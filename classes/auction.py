81a525a2107132936a964ba4a7d330555646a7f9
Stuart Lewis <stuart.lewis3110@gmail.com>
2022-06-05 00:45:53 +0100

    Add class description.

--- classes/auction.py
+++ classes/auction.py
@@ -1,3 +1,7 @@
 class Auction:
-
+    '''
+    The Auction class orchestrates an auction. It maintains a list of participating Teams, a list of players available
+    in the auction, keeps a transaction log throughout the auction, and calls the validation logic when a player
+    nomination is made.
+    '''
     pass
