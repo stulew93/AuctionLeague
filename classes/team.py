2792bca7fbb9139219de6eba7bd2292953d2dc04
Stuart Lewis <stuart.lewis3110@gmail.com>
2022-06-04 23:48:15 +0100

    Add methods to team.py file; comment out test for update_max_bid.

--- classes/team.py
+++ classes/team.py
@@ -1,7 +1,32 @@
+from classes.squad import Squad
+
 class Team:
     '''
     The Team class is a shell for the Squad - set up like this as in future I hope to add configurable elements
     (such as managers). The Team object will manage the budget for the team, including keeping track of the remaining
     budget, and whether the team is complete.
     '''
-    pass
+
+    def __init__(self, name, remaining_budget=100, target_squad_size=11):
+        self.name = name
+        self.squad = Squad()
+        self.remaining_budget = remaining_budget  # Default 100.
+        self.target_squad_size = target_squad_size  # Default 11.
+        self.team_complete = False
+
+    def update_remaining_budget(self, successful_bid):
+        # If the team has a successful bid for a player, subtract the bid from the remaining budget.
+        self.remaining_budget -= successful_bid
+        return
+
+    def check_team_complete(self):
+        # Currently the only criteria is that
+        if self.squad.current_squad_size == self.target_squad_size:
+            self.team_complete = True
+        return
+
+    def add_squad_member(self, player):
+
+        self.squad.add_player_to_squad(player)
+
+        self.check_team_complete()
